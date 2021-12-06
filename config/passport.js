7d03c8eb7d7fcf121d38ea8cf18160ac0a1e71af
VasukannanS <vasukannan2996@gmail.com>
2021-12-06 09:45:21 -0800

    callback url

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ const passport=require('passport')
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:'/auth/google/callback'
+    callbackURL:'https://vasu-google-auth.herokuapp.com/auth/google/callback'
 },
 async (accessToken,refreshToken,profile,done)=>{
     const newUser={
